89c1a7ed5160b4b85e553e18f2fa3428cbcc05d6
Samiksha <shende.samiksha@rediffmail.com>
2021-03-31 11:25:01 +0530

    [Samiksha].Add code to add weekday and weekend rates for hotel

--- src/main/java/com/bridgelabz/hotelreservation/Hotel.java
+++ src/main/java/com/bridgelabz/hotelreservation/Hotel.java
@@ -4,10 +4,18 @@ public class Hotel {
 
     private final String name;
     private final Double rates;
+    private final Double weekendRates;
 
     public Hotel(String name, Double rates) {
         this.name = name;
         this.rates = rates;
+        weekendRates = null;
+    }
+
+    public Hotel (String name, Double rates, Double weekendRates){
+        this.name = name;
+        this.rates = rates;
+        this.weekendRates = weekendRates;
     }
 
     public String getName() {
@@ -18,11 +26,16 @@ public class Hotel {
         return rates;
     }
 
+    public Double getWeekendRates() {
+        return weekendRates;
+    }
+
     @Override
     public String toString() {
         return "Hotel{" +
                 "name='" + name + '\'' +
                 ", rates=" + rates +
+                ", weekendRates=" + weekendRates +
                 '}';
     }
 }
--- src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
+++ src/main/java/com/bridgelabz/hotelreservation/HotelReservation.java
@@ -25,4 +25,10 @@ public class HotelReservation {
         int index = cheapRateHotels.indexOf(cheap);
         return hotelList.get(index).getName();
     }
+
+    public boolean addHotelRates(String name, double weekday, double weekend ) {
+        Hotel hotel = new Hotel(name, weekday, weekend);
+        hotelList.add(hotel);
+        return !hotelList.isEmpty();
+    }
 }
